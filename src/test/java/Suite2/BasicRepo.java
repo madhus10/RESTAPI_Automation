96c0c2512a895a80b40ac7ee6cb232d5595da08d
madhus10 <pinkey.madhu@gmail.com>
2019-12-26 16:46:06 +0530

    added basicrepo1 method

---
+++ src/test/java/Suite2/BasicRepo.java
@@ -0,0 +1,13 @@
+package Suite2;
+
+import org.testng.annotations.Test;
+
+public class BasicRepo {
+
+	@Test
+	public void basicrepo1() {
+		System.out.println("Printing method of basicrepo1");
+
+	}
+
+}
