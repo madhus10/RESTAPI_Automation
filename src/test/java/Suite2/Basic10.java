d66fd24f734a0716c181f2ac84c3d45be8f01071
madhus10 <pinkey.madhu@gmail.com>
2019-12-26 15:13:38 +0530

    added second testcase

--- src/test/java/Suite2/Basic10.java
+++ src/test/java/Suite2/Basic10.java
@@ -3,12 +3,19 @@ package Suite2;
 import org.testng.annotations.Test;
 
 public class Basic10 {
-	@Test
+	@Test(priority=1)
 	public void test1() {
 		System.out.println("Adding jira1");
 		System.out.println("Adding jira2");
 		System.out.println("Adding jira3");
 
 	}
+	
+	@Test(priority=2)
+	public void test2() {
+		System.out.println("Adding jira1");
+		System.out.println("Adding jira2");
+		System.out.println("Adding jira3");
 
+	}
 }
