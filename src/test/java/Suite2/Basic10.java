e77123c93bbe40dba8fdfbd77f0d6a79323c9a6a
madhus10 <pinkey.madhu@gmail.com>
2019-12-26 15:08:37 +0530

    added from asian time

--- src/test/java/Suite2/Basic10.java
+++ src/test/java/Suite2/Basic10.java
@@ -1,5 +1,14 @@
 package Suite2;
 
+import org.testng.annotations.Test;
+
 public class Basic10 {
+	@Test
+	public void test1() {
+		System.out.println("Adding jira1");
+		System.out.println("Adding jira2");
+		System.out.println("Adding jira3");
+
+	}
 
 }
